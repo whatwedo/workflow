67d30c18a9935255a05f9057a5237babc6f6dbc7
Maurizio Monticelli <maurizio@whatwedo.ch>
2020-12-03 23:18:55 +0100

    feat: let symfony/workflow and workflow bundle coexist

--- src/Service/WorkflowService.php
+++ src/Service/WorkflowService.php
@@ -23,20 +23,17 @@ class WorkflowService extends Registry
     /** @var WorkflowSubscriber */
     private $workflowListener;
 
+    private $workflows = [];
+
 
     public function __construct(WorkflowManager $manager, WorkflowSubscriber $workflowListener)
     {
-
-     //   return;
-
         $this->manager = $manager;
         $this->workflowListener = $workflowListener;
 
         try {
-
             /** @var Workflow[] $workflows */
             $workflows = $this->manager->getAllWorkflows();
-
 
             foreach ($workflows as $workflow) {
                 $definition = $this->manager->getDefinition($workflow);
@@ -63,5 +60,74 @@ class WorkflowService extends Registry
             echo   '    Update Workflow Tables    ';
             echo "\e[0m" . PHP_EOL . PHP_EOL;
         }
+    }
+
+
+    public function addWorkflow(WorkflowInterface $workflow, WorkflowSupportStrategyInterface $supportStrategy)
+    {
+        $this->workflows[] = [$workflow, $supportStrategy];
+    }
+
+    public function has(object $subject, string $workflowName = null): bool
+    {
+        foreach ($this->workflows as [$workflow, $supportStrategy]) {
+            if ($this->supports($workflow, $supportStrategy, $subject, $workflowName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @return Workflow
+     */
+    public function get(object $subject, string $workflowName = null)
+    {
+        $matched = [];
+
+        foreach ($this->workflows as [$workflow, $supportStrategy]) {
+            if ($this->supports($workflow, $supportStrategy, $subject, $workflowName)) {
+                $matched[] = $workflow;
+            }
+        }
+
+        if (!$matched) {
+            throw new InvalidArgumentException(sprintf('Unable to find a workflow for class "%s".', get_debug_type($subject)));
+        }
+
+        if (2 <= \count($matched)) {
+            $names = array_map(static function (WorkflowInterface $workflow): string {
+                return $workflow->getName();
+            }, $matched);
+
+            throw new InvalidArgumentException(sprintf('Too many workflows (%s) match this subject (%s); set a different name on each and use the second (name) argument of this method.', implode(', ', $names), get_debug_type($subject)));
+        }
+
+        return $matched[0];
+    }
+
+    /**
+     * @return Workflow[]
+     */
+    public function all(object $subject): array
+    {
+        $matched = [];
+        foreach ($this->workflows as [$workflow, $supportStrategy]) {
+            if ($supportStrategy->supports($workflow, $subject)) {
+                $matched[] = $workflow;
+            }
+        }
+
+        return $matched;
+    }
+
+    private function supports(WorkflowInterface $workflow, WorkflowSupportStrategyInterface $supportStrategy, object $subject, ?string $workflowName): bool
+    {
+        if (null !== $workflowName && $workflowName !== $workflow->getName()) {
+            return false;
+        }
+
+        return $supportStrategy->supports($workflow, $subject);
     }
 }
